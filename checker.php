70bd4783707832d30a794a917d6468b704ecdabf
arhizsx <arhizsx@gmail.com>
2024-06-14 15:33:36 +0800

    Checker

--- checker.php
+++ checker.php
@@ -69,6 +69,7 @@ $conn->close();
                 <?php 
                     }
                 ?>
+                <a class="w-100 btn btn-lg" href="/" style="background-color: #ff4bce !important; color: white;">Check Another</a>
             </form>
         </main>
     </body>
