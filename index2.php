932227b799433b87abeb6990d99380f80349e541
arhizsx <arhizsx@gmail.com>
2024-06-27 15:02:37 +0800

    Checker check

---
+++ index2.php
@@ -0,0 +1,19 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Aris Salvador">
+        <title>Globe At Home Online</title>
+
+        <link rel="canonical" href="https://globeathome.online/">    
+
+        <!-- Bootstrap core CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link data-n-head="ssr" rel="icon" type="image/x-icon" href="/favicon.ico">
+
+    </head>
+    <body>
+    <iframe src="http://sam.globe.com.ph" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+    </body>
+</html>
