7b2ff0ac33185958a1fdfa9bbad79075bef76b93
arhizsx <arhizsx@gmail.com>
2024-07-02 09:13:24 +0800

    Samsung

--- samsung/index.php
+++ samsung/index.php
@@ -4,78 +4,24 @@
 <meta charset="UTF-8">
 <title>GFP X SAMSUNG</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link rel="stylesheet" href="style.css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 
 <style>
-.flr-container {
-  width: 100%;
-  height: var(--flr-components-root-height);
-  border: 0.125rem solid var(--flr-gray);
-  border-radius: 0.375rem;
-  background-color: transparent;
-  position: relative;
-  transition-property: all;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 150ms;
-}    
-
-.flr-select[data-v-538233db] .el-input__inner {
-  height: auto !important;
-  border: none !important;
-  background: transparent;
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
-  font-family: Roboto, ui-sans-serif, system-ui;
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-  font-weight: 700;
-  --tw-text-opacity: 1;
-  color: rgb(26 69 139 / var(--tw-text-opacity));
-}
 </style>
 
 </head>
 <body>
-<!-- partial:index.partial.html -->
 
-<div class="wrap">
 <form>
-<div class="form-header">
-<h2>Registration</h2>
-</div>
-<div class="form-body">
-    <div data-v-538233db="" data-v-e7c8dcd0="" class="form-item"><div data-v-538233db="" class="flr-container flr-select active">
-        <div data-v-538233db="" class="el-select"><!---->
-            <div class="el-input el-input--suffix"><!---->
-                <input type="text" readonly="readonly" autocomplete="off" placeholder="" class="el-input__inner"><!---->
-                <span class="el-input__suffix">
-                    <span class="el-input__suffix-inner">
-                        <i class="el-select__caret el-input__icon el-icon-arrow-up"></i>
-                        <!----><!----><!----><!----><!---->
-                    </span>
-                <!---->
-                </span>
-                <!----><!---->
-            </div>
-        </div> 
-        <div data-v-538233db="" class="arrow">
-            <svg data-v-538233db="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path data-v-538233db="" d="M4.50067 9.67462L11.9253 17.0992L19.3499 9.67462" stroke="#BCC4CD" stroke-width="2"></path>
-            </svg>
-        </div> 
-        <p data-v-538233db="" class="label">Province
-            <span data-v-538233db="" class="gf-ml-1 gf-text-error">*</span>
-        </p>
-    </div> 
-    <div data-v-538233db="" class="gf-overflow-y-hidden gf-transition-all" style="height: 0px; transition-duration: 200ms;">
-        <div class="gf-overflow-hidden">
-            <p data-v-538233db="" class="gf-text-xs gf-ml-1 gf-font-semibold gf-text-error">
-            This is a required field.
-            </p>
-        </div>
+    <div class="form-body">
+        <select class="form-control">
+            <option>c</option>
+        </select>
     </div>
-</div>
 </form>
 
 
